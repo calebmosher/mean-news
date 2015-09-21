788346df18be4721c472d7e43d451daeb2127fa6
Caleb Mosher <caleb_mosher@apple.com>
2015-09-21 15:43:59 -0700

    Add new comment submission

--- public/app.js
+++ public/app.js
@@ -18,61 +18,129 @@ app
 	
 	.factory("posts", [function() {
 		var posts = {
-			posts: [ ]
+			posts: [
+				{
+					title: "Ut pretium eros nec lorem lacinia faucibus",
+					url: "http://example.com",
+					upvotes: 5,
+					description: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+					comments: [
+						{
+							author: "Louis CK",
+							body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+							upvotes: 0
+						},
+						{
+							author: "Bo Burnham",
+							body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+							upvotes: 0
+						},
+						{
+							author: "Bill Burr",
+							body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+							upvotes: 0
+						}
+					]
+				},
+				{
+					title: "Phasellus non lacus congue, sagittis justo tincidunt",
+					url: "http://example.com",
+					upvotes: 9,
+					description: "Quisque vulputate ante sit amet nibh sagittis sagittis. Etiam et nisi nisi. Nam vulputate ornare eros, et imperdiet augue congue nec.",
+					comments: [
+						{
+							author: "Louis CK",
+							body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+							upvotes: 0
+						},
+						{
+							author: "Bo Burnham",
+							body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+							upvotes: 0
+						},
+						{
+							author: "Bill Burr",
+							body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+							upvotes: 0
+						}
+					]
+				},
+				{
+					title: "Nunc facilisis in erat ut blandit",
+					url: "http://example.com",
+					upvotes: 2,
+					description: "Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+					comments: [
+						{
+							author: "Louis CK",
+							body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+							upvotes: 0
+						},
+						{
+							author: "Bo Burnham",
+							body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+							upvotes: 0
+						},
+						{
+							author: "Bill Burr",
+							body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+							upvotes: 0
+						}
+					]
+				},
+				{
+					title: "Suspendisse iaculis velit augue",
+					url: "http://example.com",
+					upvotes: 13,
+					description: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim.",
+					comments: [
+						{
+							author: "Louis CK",
+							body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+							upvotes: 0
+						},
+						{
+							author: "Bo Burnham",
+							body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+							upvotes: 0
+						},
+						{
+							author: "Bill Burr",
+							body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+							upvotes: 0
+						}
+					]
+				},
+				{
+					title: "Donec laoreet dui nec malesuada condimentum",
+					url: "http://example.com",
+					upvotes: 4,
+					description: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+					comments: [
+						{
+							author: "Louis CK",
+							body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
+							upvotes: 0
+						},
+						{
+							author: "Bo Burnham",
+							body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
+							upvotes: 0
+						},
+						{
+							author: "Bill Burr",
+							body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
+							upvotes: 0
+						}
+					]
+				},
+			]
 		};
 		return posts;
 	}])
 	
 	.controller("MainCtrl", ["$scope", "posts", function($scope, posts) {
-		$scope.posts = posts.posts = [
-			{
-				title: "Ut pretium eros nec lorem lacinia faucibus",
-				url: "http://example.com",
-				upvotes: 5,
-				description: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin."
-			},
-			{
-				title: "Phasellus non lacus congue, sagittis justo tincidunt",
-				url: "http://example.com",
-				upvotes: 9,
-				description: "Quisque vulputate ante sit amet nibh sagittis sagittis. Etiam et nisi nisi. Nam vulputate ornare eros, et imperdiet augue congue nec."
-			},
-			{
-				title: "Nunc facilisis in erat ut blandit",
-				url: "http://example.com",
-				upvotes: 2,
-				description: "Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis."
-			},
-			{
-				title: "Suspendisse iaculis velit augue",
-				url: "http://example.com",
-				upvotes: 13,
-				description: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim."
-			},
-			{
-				title: "Donec laoreet dui nec malesuada condimentum",
-				url: "http://example.com",
-				upvotes: 4,
-				description: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
-				comments: [
-					{
-						author: "Louis CK",
-						body: "Curabitur rhoncus lorem eu dignissim condimentum. Mauris laoreet, metus id sodales ornare, justo magna laoreet erat, sed posuere massa tellus sed lectus.",
-						upvotes: 0
-					},
-					{
-						author: "Bo Burnham",
-						body: "Suspendisse vulputate sem et mauris convallis consequat. Donec posuere sit amet velit sed sollicitudin.",
-						upvotes: 0
-					},
-					{
-						author: "Bill Burr",
-						body: "Phasellus vitae velit facilisis, vehicula diam id, blandit est. Integer sed mauris enim. Curabitur fermentum eros et leo dictum, eu fermentum velit vulputate. Sed pellentesque urna nec posuere aliquam. Praesent semper fringilla enim ac sagittis.",
-						upvotes: 0
-					}
-				]
-			},
-		];
+		$scope.posts = posts.posts;
 		
 		$scope.addPost = function() {
 			if (!$scope.title) return;
@@ -120,7 +188,19 @@ app
 	.controller("PostCtrl", ["$scope", "$stateParams", "posts", function($scope, $stateParams, posts) {
 		$scope.post = posts.posts[$stateParams.id];
 		
-		$scope.upvote = function(comment, $event) {
+		$scope.upvotePost = function(post, $event) {
+			var target = $event.target.classList.contains("votes") ? $event.target : $event.target.parentElement;
+			
+			if (target.classList.contains("voted")) {
+				post.upvotes--;
+				target.classList.remove("voted");
+				return;
+			}
+			
+			post.upvotes++;
+			target.classList.add("voted");
+		};
+		$scope.upvoteComment = function(comment, $event) {
 			var target = $event.target;
 			
 			if (target.classList.contains("voted")) {
@@ -131,5 +211,17 @@ app
 			
 			comment.upvotes++;
 			target.classList.add("voted");
+		};
+		
+		$scope.addComment = function() {
+			if (!$scope.body) return;
+			
+			$scope.post.comments.push({
+				author: $scope.name || "Anonymous",
+				body: $scope.body,
+				upvotes: 0
+			});
+			
+			$scope.name = $scope.body = "";
 		};
 	}]);
